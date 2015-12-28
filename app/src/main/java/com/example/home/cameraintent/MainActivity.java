8e3c12f80088875e18ee5e3aeb29232ac99fae98
kbhardwj <kaushalbhardwaj12@gmail.com>
2015-12-29 00:25:12 +0530

    initial commit2

---
+++ app/src/main/java/com/example/home/cameraintent/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.home.cameraintent;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt;
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv=(ImageView)findViewById(R.id.imageView);
+        bt=(Button)findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bp=(Bitmap)data.getExtras().get("data");
+        iv.setImageBitmap(bp);
+    }
+}
